fcbd412111dff65232ab1e1f2506306faafc6a42
abhiyadav-2307 <abhi1611919@gmail.com>
2025-07-02 07:24:21 +0530

    MongoDB and Stream SDK setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -14,4 +15,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`server is running on port ${PORT}`);
+    connectDB();
 })
