ee9479e25cb4ba55369194163c6b7e3b82670713
abhiyadav-2307 <abhi1611919@gmail.com>
2025-07-21 15:15:44 +0530

    Completed authentication (signup, login, logout)

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,120 @@
-export function signup(req, res) {
-  res.send("Signup Route");
+import { upsertStreamUser } from "../lib/stream.js";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+export async function signup(req, res) {
+  try {
+    const { fullName, email, password } = req.body;
+    if (!email || !fullName || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    //random profile pic/Avatar for user
+    const idx = Math.floor(Math.random() * 100) + 1; //random number between 1-100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = await User.create({
+      fullName,
+      email,
+      password,
+      profilePic: randomAvatar,
+    });
+
+    try {
+      await upsertStreamUser({
+        id: newUser._id.toString(),
+        name: newUser.fullName,
+        image: newUser.profilePic || "",
+      });
+      console.log(`Stream User created for ${newUser.fullName}`);
+    } catch (error) {
+      console.log(`Error creating or upserting ${newUser.fullName}: `, error);
+    }
+
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, // prevent XSS attacks,
+      sameSite: "strict", // prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production",
+    });
+    const { password: userPassword, ...rest } = newUser._doc; //send whole user except password
+
+    return res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: rest,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 }
-export function login(req, res) {
-  res.send("Login Route");
+
+export async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordCorrect = await user.matchPassword(password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, // prevent XSS attacks,
+      sameSite: "strict", // prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    const { password: userPassword, ...rest } = user._doc; //send whole user except password
+
+    return res.status(200).json({
+      success: true,
+      message: "Logged In successfully",
+      user: rest,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 }
+
 export function logout(req, res) {
-  res.send("Logout Route");
+  res.clearCookie("jwt");
+  return res
+    .status(200)
+    .json({ success: true, message: "Logged out successfully" });
 }
