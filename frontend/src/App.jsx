e61f03e9813d5d09a75d8798a0f7e77031285325
abhiyadav-2307 <abhi1611919@gmail.com>
2025-07-25 09:41:51 +0530

    React-router-dom, cors and authenticated navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage.jsx";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
+import OnboardingPage from "./Pages/OnboardingPage";
+import CallPage from "./Pages/CallPage.jsx";
+import NotificationPage from "./Pages/NotificationsPage.jsx";
+import ChatPage from "./Pages/ChatPage.jsx";
+import toast, { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { axiosInstance } from "./lib/axios.js";
 
 const App = () => {
+
+  // Fetch the authenticated user
+  const { data:authData, isLoading, error } = useQuery({
+    queryKey: ["authuser"],
+    queryFn: async () => {
+      const res = await axiosInstance("/auth/me");
+      return res.data;
+    },
+    retry: false,
+  });
+  const authUser = authData?.data;
+
+
   return (
     <div data-theme="dim">
-      <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
-    </div>
-  )
-}
+      <Routes>
+        <Route path="/" element={ authUser ? <HomePage /> : <Navigate to="/login"/>} />
+        <Route path="/signup" element={!authUser ? <SignupPage /> : <Navigate to="/"/>} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/"/>} />
+        <Route path="/onboarding" element={authUser ? <OnboardingPage /> : <Navigate to="/login"/>} />
+        <Route path="/call" element={authUser ? <CallPage /> : <Navigate to="/login"/>} />
+        <Route path="/notifications" element={authUser ? <NotificationPage /> : <Navigate to="/login"/>} />
+        <Route path="/chat" element={authUser ? <ChatPage /> : <Navigate to="/login"/>} />
+      </Routes>
 
-export default App
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='bg-red-200'>HomePage</div>
+  )
+}
+
+export default HomePage
