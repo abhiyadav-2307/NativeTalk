799118ec3c00448235766c3c2d50b4d01b600972
abhiyadav-2307 <abhi1611919@gmail.com>
2025-08-06 19:54:35 +0530

    Deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const Base_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: Base_URL,
   withCredentials: true, // This allows cookies to be sent with requests
 });
